f0cc48e683b1cf0619a8c4f45ca8d4fa9a05e854
drew <sirampr@gmail.com>
2022-08-04 15:42:10 -0400

    Fix gradient background

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,6 +17,7 @@ export default function Layout({ children, title }: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="max-w-4xl mx-auto p-4">
+        <div className="gradient fixed top-0 left-0 right-0 bottom-0" />
         <Navbar />
         {children}
         <Footer />
